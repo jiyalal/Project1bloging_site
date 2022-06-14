926439e97a7d019aab914172e4fd3f6507e7d471
Bidipta-BG <gogoibidipta@gmail.com>
2022-06-14 23:05:38 +0530

    assignment submission auth1

---
+++ src/middlewares/tokenAuthentication.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const tokenAuthenticator = async function (req, res, next) {
+  let token = req.headers["x-Auth-token"];
+  if (!token) {
+    token = req.headers["x-auth-token"];
+  }
+
+  if (!token) {
+    return res.send({
+      status: false,
+      msg: "token must be present",
+    });
+  }
+
+  let decodedToken = jwt.verify(token, "functionup-radon-1");
+  if (!decodedToken) {
+    return res.send({
+      status: false,
+      msg: "token is invalid",
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports.tokenAuthenticator = tokenAuthenticator;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const tokenAuth = require("../middlewares/tokenAuthentication")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+
 
 router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",tokenAuth.tokenAuthenticator, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",tokenAuth.tokenAuthenticator, userController.updateUser)
+
+router.delete("/users/:userId",tokenAuth.tokenAuthenticator, userController.deleteUser);
+
 
 module.exports = router;
