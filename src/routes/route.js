cb71b573fbb9a49c101cbfa89605ae640fba31c5
Bidipta-BG <gogoibidipta@gmail.com>
2022-06-21 12:29:53 +0530

    project1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
-const tokenAuth = require("../middlewares/tokenAuthentication")
 
 
 
-router.post("/users", userController.createUser  )
-
-router.post("/login", userController.loginUser)
-
-router.get("/users/:userId",tokenAuth.tokenAuthenticator, userController.getUserData)
-
-router.put("/users/:userId",tokenAuth.tokenAuthenticator, userController.updateUser)
-
-router.delete("/users/:userId",tokenAuth.tokenAuthenticator, userController.deleteUser);
 
 
 module.exports = router;
